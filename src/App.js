8e100e6981f68e6f30194a4f13fd3773b3e264d3
sonias02 <sonias02@cs.washington.edu>
2020-09-21 21:45:20 -0700

    create page to test resource rendering

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import soniaPage from "./components/interns/soniaPage";
 import AdilPage from "./components/interns/AdilPage";
 import CreateAccount from "./components/CreateAccount";
 import NavBarComponent from "./components/NavBarComponent"
+import TestResources from "./components/TestResources";
 
 /** React-redux Components */
 import { Provider } from 'react-redux';
@@ -40,6 +41,7 @@ const App = () => {
             <Route path="/interns/soniaPage" component={soniaPage} />
             <Route path="/interns/AdilPage" component={AdilPage} />
             <Route path="/CreateAccount" component={CreateAccount} />
+            <Route path="/TestResources" component={TestResources} />
             <Route component={PageNotFound} />
           </Switch>
         </Container>
---
+++ src/components/TestResources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Jumbotron} from "react-bootstrap";
+import ResourceComponent from './ResourceComponent';
+
+
+function TestResources() {
+
+  return (
+      <Jumbotron>
+        <ResourceComponent/>
+      </Jumbotron>
+  );
+}
+
+export default TestResources;
