0bcb11a355dab249859a332e18bba2d4984634c3
sonias02 <sonias02@cs.washington.edu>
2020-09-22 00:35:55 -0700

    resource grid card/list layout

---
+++ src/components/ResourceGrid.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+import {Button, Row, Col, Container} from "react-bootstrap";
+import '../styles/ResourceGrid.css';
+
+
+const ResourceGrid = ({resources, type}) => {
+
+    function listType() {
+        return(
+            <Container>
+                <Row xs={1} className="list">
+                    {resources.map((resource, i) => (
+                        <Col key={i} className="list-item">
+                            <h1>{resource.name}</h1>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        );
+    }
+
+    function cardType() {
+        return(
+            <Container>
+                <Row xs={1} sm={2} md={3} lg={4}>
+                    {resources.map((resource, i) => (
+                        <Col key={i} className="card">
+                            <div className="content">
+                                <Image src={resource.image}/>
+                                <h1>{resource.name}</h1>
+                                <p>{resource.description}</p>
+                                <Button>SEE MORE</Button>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        );
+    }
+
+    return (
+        <div className="resource-grid">
+            {type === "list" ? listType() : cardType()}
+        </div>
+    );
+};
+
+export default ResourceGrid;
--- src/components/TestResources.js
+++ src/components/TestResources.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import {Jumbotron} from "react-bootstrap";
-import ResourceComponent from './ResourceComponent';
+import ResourceGrid from './ResourceGrid';
 
 
 function TestResources() {
+  const resources = [
+    {
+      image: "/img/BLM.png",
+      name:"Ut tempus",
+      description: "Ut tempus facilisis lacinia. Maecenas pharetra vel orci vitae tempor.",
+    },
+    {
+      image: "/img/BLM.png",
+      name:"Excepteur",
+      description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt ollit anim id est laborum."
+    },
+    {
+      image: "/img/BLM.png",
+      name:"Orci varius",
+      description: "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+      image: "/img/BLM.png",
+      name:"Praesent",
+      description: "Praesent finibus dolor et luctus tincidunt. Phasellus ut neque eu nisl interdum luctus eu et nisi."
+    }
+  ]
 
   return (
-      <Jumbotron>
-        <ResourceComponent/>
-      </Jumbotron>
+      // <ResourceGrid resources={resources} type={"card"}/>
+      <ResourceGrid resources={resources} type={"list"}/>
   );
 }
 
