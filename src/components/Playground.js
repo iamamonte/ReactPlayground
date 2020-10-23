299708ec7cdd3ed6aacb346b8710c1acf186e0b5
iamamonte <iamamonte@gmail.com>
2020-10-23 12:27:57 -0400

    Update Playground.js
    Cleaning up playground.

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -6,8 +6,8 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button"
-import {Modal} from "react-bootstrap"
+// import Button from "react-bootstrap/Button"
+// import {Modal} from "react-bootstrap"
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -21,152 +21,27 @@ import FadeText from "./practice/FadeText";
 import PopOverTrigger from "./practice/PopOverButton";
 import LanguageList from './practice/LanguageList';
 import JokeModal from "./practice/JokeModal";
-import Counter from './practice/Counter';
-import DisplayEvent from './DisplayEvent';
 
 /** Components Continued */
-
 import PeopleComponent from "./PeopleComponent";
-import CreatePost from "./CreatePost"
-import ConfirmationBox from "./ConfirmationBox";
-import MissionsCarousel from "./MissionsCarousel"
+// import ConfirmationBox from "./ConfirmationBox";
 
 
 /* Models/Services */
-import {Intern} from "../model/Intern";
-
+import {Interns, Profiles} from '../data/dummyData'
 
 // import JokeModal from "./JokeModal";
 
-import profilePic from '../resources/profilePicture.jpeg'
-import DisplayPost from "./DisplayPost";
-import Icon from "./Icon";
-import {BsFillAlarmFill} from 'react-icons/bs';
 
-let amonte = new Intern(
-  "Amonte",
-  "Just a regular Joe named Amonte.",
-  "Intern Lead",
-  "https://cdn.ebaumsworld.com/mediaFiles/picture/730195/86187705.jpg",
-  new Date("1/1/21")
-);
-let sanjana = new Intern(
-  "Sanjana",
-  "Sophomore at UT Austin",
-  "Backend Web/Intern Lead",
-  "https://picjumbo.com/wp-content/uploads/free-stock-photos-san-francisco-1080x720.jpg",
-  new Date("8/21/20")
-);
-let shayan = new Intern(
-  "Shayan",
-  "may the force be with you.",
-  "Frontend Web/Intern Lead",
-  "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
-  new Date("9/8/20")
-);
-
-let henry = new Intern(
-  "Henry",
-  "From New York",
-  "Backend Web Intern",
-  "https://www.history.com/.image/t_share/MTU3ODc5MDgyNjY5OTc1MjYz/new-york-city.jpg",
-  new Date("9/1/20"));
+let amonte = Interns.amonte, sanjana = Interns.sanjana, 
+shayan = Interns.shayan, sonia = Interns.sonia, adil = Interns.adil, kyle = Interns.kyle, 
+henry = Interns.henry, disha = Interns.disha, luis = Interns.luis;
 
 
-let sonia = new Intern(
-  "Sonia",
-  "Rising Sophomore at UW",
-  "Intern (Web/Mobile Dev)",
-  "https://cdn.pixabay.com/photo/2015/07/17/18/01/portland-bill-849583_960_720.jpg",
-  new Date("9/25/20")
-);
-
-let disha = new Intern(
-  "Disha",
-  "Believe in yourself",
-  "Intern (Mobile Developer)",
-  "https://wallpaperaccess.com/full/142733.jpg",
-  new Date("8/30/20")
-);
-let adil = new Intern(
-  "Adil ",
-  "Rising Junior at UMB",
-  "Intern (Web/Mobile Dev)",
-  "https://www.cloudcms.com/images/quickstarts/react/react.df70b005.png",
-  new Date("7/20/20")
-);
-
-let kyle = new Intern(
-  "Kyle",
-  "Rising Sophomore at Grinnell College",
-  "Intern (Web/Mobile Dev)",
-  "https://www.psdstack.com/wp-content/uploads/2016/10/featured-copyright-free-mages.jpg",
-  new Date("8/20/20")
-);
-
-let luis = new Intern(
-  "Luis",
-  "Senior at George Mason University",
-  "Intern (FrontEnd Web)",
-  "https://i.pinimg.com/600x315/88/5b/7b/885b7b783181234c51ea17ec48a02506.jpg",
-  new Date("8/30/20")
-);
-
-const items = [
-  {
-    img: "/img/BLM.png",
-    alt:"blm",
-    caption: "TurnUp believes People’s Rights are Human Rights and Human Rights are People’s Rights",
-  },
-  {
-    img: "/img/BLM.png",
-    alt:"blm",
-    caption: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt ollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-  },
-  {
-    img: "/img/BLM.png",
-    alt:"blm",
-    caption: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt ollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-  }
-]
-
-let kyleProfile = {
-  firstName: "Kyle",
-  lastName: "Kobayashi",
-  city: "Miami",
-  state: "FL",
-  country: "US",
-  profileImage:profilePic
-};
-
-let amonteProfile = {
-  firstName: "Amonte",
-  lastName: "Andrews",
-  city: "St Petersburg",
-  state: "FL",
-  country: "US",
-  profileImage:"https://cdn.ebaumsworld.com/mediaFiles/picture/730195/86187705.jpg"
-};
-
-const event = {
-  name: "Lorem ipsum dolor sit amet",
-  img: "https://cdn.pixabay.com/photo/2017/08/03/11/05/people-2575608_960_720.jpg",
-  description: "Ut tempus facilisis lacinia. Maecenas pharetra vel orci vitae tempor. Nulla sit amet ullamcorper ipsum. Vivamus vestibulum massa tortor, at luctus leo auctor ac. Praesent finibus dolor et luctus tincidunt. Phasellus ut neque eu nisl interdum luctus eu et nisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi tempor sapien at faucibus mattis. Quisque venenatis tellus sed diam facilisis tempor. Vivamus ut mi at quam ultricies posuere. Maecenas in ipsum id quam maximus faucibus.",
-  tags: ["Activism", "#TimeForChange"],
-  date: "Sunday, May 25, 2020",
-  time: "8:00 AM - 12:00 PM",
-  location: "Washington Square Park"
-}
-
-let postData = {
-  content: "Ut tempus facilisis lacinia. Maecenas pharetra vel orci vitae tempor. Nulla sit amet ullamcorper ipsum. Vivamus vestibulum massa tortor, at luctus leo auctor ac. Praesent finibus dolor et luctus tincidunt. Phasellus ut neque eu nisl interdum luctus eu et nisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi tempor sapien at faucibus mattis. Quisque venenatis tellus sed diam facilisis tempor. Vivamus ut mi at quam ultricies posuere. Maecenas in ipsum id quam maximus faucibus.",
-  time: new Date("August 25, 2020 03:24:00"),
-  imgs: ["https://cdn.pixabay.com/photo/2017/08/03/11/05/people-2575608_960_720.jpg","https://cdn.pixabay.com/photo/2017/08/03/11/05/people-2575608_960_720.jpg", "https://cdn.pixabay.com/photo/2017/08/03/11/05/people-2575608_960_720.jpg","https://cdn.pixabay.com/photo/2017/08/03/11/05/people-2575608_960_720.jpg"],
-  //video: "https://www.youtube.com/embed/ttIWUvxnuEo",
-  stats: {likes: 162, reposts: 38, comments:52, favorites: 123}
-}
+let amonteProfile = Profiles.amonte, kyleProfile = Profiles.kyle;
 
 
+/*
 const ConfirmModal = ({header, content}) => {
   const [show, setShow] = useState(false);
 
@@ -189,21 +64,13 @@ const ConfirmModal = ({header, content}) => {
         <Button  className="d-flex justify-content-center" style={{backgroundColor:"#402366", color:"white",width:"80%",margin:"0 auto 0 auto"}}  onClick={handleClose}>
             Okay
           </Button>
-
-
         </Modal.Body>
-        {/* <Modal.Footer>
-
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer> */}
       </Modal>
     </>
   );
 
 }
-
+*/
 
 
 
@@ -215,32 +82,15 @@ const Playground = (props) => {
 
   return (
     <Container>
-  
       <Jumbotron>
-        <DisplayEvent event={event} profile={amonteProfile}/>
         <h1 className="header">TurnUp React Playground</h1>
         <ToastWrapper title="Welcome!">
         <span role="img" aria-label="tada">
           🎉 🎉 🎉 🎉
         </span>
-        <Icon color={"gray"}
-            onClick={() => {alert("icon click") }}>
-              <BsFillAlarmFill /></Icon>
+      
         </ToastWrapper>
-        <Row className='justify-content-center'>
-                <ConfirmModal header={"Post Successful!"} content="Lorem ipsum ...." />
-        </Row>
-        <Counter></Counter>
         <Container>
-          <Row>
-            <Col><MissionsCarousel items={items} header={"Welcome to Turnup!"}></MissionsCarousel></Col>
-            <Col><div>Login Component</div></Col>
-          </Row>
-        </Container>
-        <Container>
-          
-         <DisplayPost post={postData} profile={amonteProfile}/>
-        <CreatePost profile={amonteProfile}/>
           <Row xs={1} md={2} lg={3} className='justify-content-center'>
             <Col className="my-3">
               <InternInfoDisplay
