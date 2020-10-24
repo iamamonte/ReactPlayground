a995a13d86163c1eefb502ddf81f418f915528d1
sonias02 <sonias02@cs.washington.edu>
2020-10-24 16:20:10 -0700

    bootstrap layout

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -8,9 +8,37 @@ import {BsCalendar, BsClockFill, BsFillTagFill} from 'react-icons/bs';
 const EventDetails = ({event}) => {
 
   return (
-    <div>
+    <Container>
+      <Row xs={1} md={2}>
+        <Col>
+          <Row xs={1}>
+            <Col>Image</Col>
+            <Col>Title</Col>
+            <Col>Profile</Col>
+            <Col>About</Col>
+            <Col>Description</Col>
+          </Row>
+        </Col>
+          <Row xs={1}>
+            <Col>Map</Col>
+            <Col>
+              <Row xs={1} sm={2}>
+                <Col>Details</Col>
+                <Col>Tags</Col>
+              </Row>
+            </Col>
+            <Col>
+              <Row xs={1} sm={2}>
+                <Col>Invite Button</Col>
+                <Col>RSVP Button</Col>
+              </Row>
+            </Col>
+          </Row>
+        <Col>
 
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
