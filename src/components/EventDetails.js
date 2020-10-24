593557c083448fb8485d5a16bfc209204a93a64a
sonias02 <sonias02@cs.washington.edu>
2020-10-24 15:52:36 -0700

    setup component and testing page

---
+++ src/components/EventDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import PeopleComponent from "./PeopleComponent";
+import {ImLocation} from 'react-icons/im';
+import {BsCalendar, BsClockFill, BsFillTagFill} from 'react-icons/bs';
+
+
+const EventDetails = ({event}) => {
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default EventDetails;
