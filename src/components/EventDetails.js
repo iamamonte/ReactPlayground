1c3d8a56a92752859059a90f25e14f48e881f89c
sonias02 <sonias02@cs.washington.edu>
2020-10-25 01:15:50 -0700

    styled layout with dummy data, small display issue

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,41 +1,77 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import PeopleComponent from "./PeopleComponent";
-import {ImLocation} from 'react-icons/im';
-import {BsCalendar, BsClockFill, BsFillTagFill} from 'react-icons/bs';
+import {BsCalendar, BsFillTagFill} from 'react-icons/bs';
+import {FiCamera} from 'react-icons/fi';
+import {Profiles} from '../data/dummyData';
+import {events} from '../data/dummydata.json';
+import '../styles/EventDetails.css';
 
+const EventDetails = () => {
 
-const EventDetails = ({event}) => {
+  // dummy data for testing
+  const event = events[0];
+  const map = "https://cdn.filestackcontent.com/qfzkumr0RE27pdC8tqeH";
+
+  // format list of tags
+  const renderedTags = event.tags.map((tag, i) =>  <p className="tag" key={i}><BsFillTagFill/>{tag}</p>);
 
   return (
-    <Container>
+    <Container className="event-details">
       <Row xs={1} md={2}>
         <Col>
           <Row xs={1}>
-            <Col>Image</Col>
-            <Col>Title</Col>
-            <Col>Profile</Col>
-            <Col>About</Col>
-            <Col>Description</Col>
+            <Col className="image"><img src={event.img} alt={event.name}/></Col>
+            <Col className="name-section">
+              <h1>{event.name}</h1>
+              <span>0 people going</span>
+            </Col>
+            <Col className="profile-section">
+              <PeopleComponent profile={Profiles.amonte}></PeopleComponent>
+              <span>Posted an event</span>
+            </Col>
+            <Col className="about-header"><p>About</p></Col>
+            <Col><p>{event.description}</p></Col>
           </Row>
         </Col>
+        <Col>
           <Row xs={1}>
-            <Col>Map</Col>
-            <Col>
+            <Col className="map"><img src={map} alt="map placeholder"/></Col>
+            <Col className="details">
               <Row xs={1} sm={2}>
-                <Col>Details</Col>
-                <Col>Tags</Col>
+                <Col>
+                  <Row>
+                    <Col className="icon" xs={1}>
+                      <BsCalendar/>
+                    </Col>
+                    <Col className="detail" xs={11}>
+                      <p className="title">{event.date}</p>
+                      <p className="time">{event.time}</p>
+                      <p className="add-to-calender">Add to Calendar</p>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="icon" xs={1}>
+                      <FiCamera/>
+                    </Col>
+                    <Col className="detail" xs={11}>
+                      <p className="title">Virtual/Zoom</p>
+                      {event.link ? <p><a href={event.link}>{event.link}</a></p> : <p>No link provided</p>}
+                      <p className="title">Organizer's Location</p>
+                      <p>{event.location}</p>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col>{renderedTags}</Col>
               </Row>
             </Col>
             <Col>
               <Row xs={1} sm={2}>
-                <Col>Invite Button</Col>
-                <Col>RSVP Button</Col>
+                <Col><Button>Invite followers</Button></Col>
+                <Col><Button>RSVP for Event</Button></Col>
               </Row>
             </Col>
           </Row>
-        <Col>
-
         </Col>
       </Row>
     </Container>
